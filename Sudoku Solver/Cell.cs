605379c1db424a1cf6ff9e730c45a9ca1b2c78b9
Jerrick Snyder <runesword85@yahoo.com>
2023-06-25 09:34:16 -0400

    C# Version Initial Commit

---
+++ Sudoku Solver/Cell.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver
+{
+    internal class Cell
+    {
+        public Cell[] row;
+        public Cell[] col;
+        public Cell[] square;
+        public string comment = string.Empty;
+        public int value;
+        public bool given;
+        public List<int> available;
+        public Point myLocation;
+
+        public Cell(int value, bool given, Point myLocation)
+        {
+            this.value = value;
+            this.given = given;
+            row = new Cell[9];
+            col = new Cell[9];
+            square = new Cell[9];
+            available = Enumerable.Range(1, 9).ToList();
+            if (given)
+                comment = $"{DateTime.Now} | Given";
+            this.myLocation = myLocation;
+        }
+
+        public void SetAvailable()
+        {
+            if (value != -1)
+            {
+                available.Clear();
+                return;
+            }
+            CheckNeighbors(row);
+            CheckNeighbors(col);
+            CheckNeighbors(square);
+        }
+
+        private void CheckNeighbors(Cell[] neighbors)
+        {
+            foreach (Cell cell in neighbors)
+            {
+                if (available.Contains(cell.value))
+                {
+                    available.Remove(cell.value);
+                }
+            }
+        }
+
+        public void SolveByScanning()
+        {
+            if (available.Count == 1)
+            {
+                value = available[0];
+                available.Clear();
+                comment = $"{DateTime.Now} | Solved by scanning";
+            }
+        }
+
+        public void SolveByMissingNumbers()
+        {
+            CheckForMissingValues(square);
+            CheckForMissingValues(row);
+            CheckForMissingValues(col);
+        }
+
+        private void CheckForMissingValues(Cell[] neighbors)
+        {
+            List<int> valuesMissing = Enumerable.Range(1, 9).ToList();
+
+            foreach (Cell cell in neighbors)
+            {
+                if (valuesMissing.Contains(cell.value))
+                {
+                    valuesMissing.Remove(cell.value);
+                }
+            }
+
+            if (valuesMissing.Count >= 3)
+                return;
+
+            foreach (int missingValue in valuesMissing)
+            {
+                List<Cell> canHoldMissingValue = new List<Cell>();
+                foreach (Cell cell in neighbors)
+                {
+                    if (cell.value == -1)
+                        continue;
+                    if (cell.available.Contains(missingValue))
+                    {
+                        canHoldMissingValue.Add(cell);
+                    }
+                }
+
+                if (canHoldMissingValue.Count == 1)
+                {
+                    canHoldMissingValue[0].value = missingValue;
+                    canHoldMissingValue[0].comment = $"{DateTime.Now} | Solved by checking for missing values";
+                }
+                    
+            }
+        }
+
+        public void SolveByOnlyAvailable()
+        {
+            CheckIfOnlyAvailable(row);
+            CheckIfOnlyAvailable(col);
+            CheckIfOnlyAvailable(square);
+        }
+
+        private void CheckIfOnlyAvailable(Cell[] neighbors)
+        {
+            foreach (int numAvailable in available)
+            {
+                List<Cell> containsSameAvailableNumber = new List<Cell>();
+                foreach (Cell cell in neighbors)
+                {
+                    if (cell.available.Contains(numAvailable))
+                        containsSameAvailableNumber.Add(cell);
+                }
+                if (!containsSameAvailableNumber.Any())
+                {
+                    value = numAvailable;
+                    comment = $"{DateTime.Now} | Solved by checking if only available number";
+                    available.Clear();
+                }    
+            }
+        }
+    }
+}
---
+++ Sudoku Solver/Solver.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver
+{
+    internal class Solver
+    {
+        private Cell[,] grid;
+        private readonly DataGridView dataGridView;
+
+        public Solver(DataGridView dataGridView)
+        {
+            this.dataGridView = dataGridView;
+            this.grid = new Cell[9, 9];
+            BuildGrid();
+            AssignCellRelatives();
+            bool stophere = true;
+        }
+
+        private void AssignCellRelatives()
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        grid[x, y].row[i] = grid[x, i];
+                        grid[x, y].col[i] = grid[i, y];
+                    }
+                    AssignSquare(x, y);
+                }
+            }
+        }
+
+        private void AssignSquare(int x, int y)
+        {
+            int starting_x = (x / 3) * 3;
+            int starting_y = (y / 3) * 3;
+
+            int index = 0;
+            for (int i = starting_x; i < starting_x + 3; i++)
+            {
+                for (var j = starting_y; j < starting_y + 3; j++)
+                {
+                    grid[x, y].square[index] = grid[i, j];
+                    index++;
+                }
+            }
+
+        }
+
+        private void BuildGrid()
+        {
+            int x = 0;
+            foreach (DataGridViewRow row in dataGridView.Rows)
+            {
+                int y = 0;
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    try
+                    {
+                        grid[x, y] = new Cell(int.Parse(cell.Value.ToString()), true, new Point(x, y));
+                    }
+                    catch
+                    {
+                        grid[x, y] = new Cell(-1, false, new Point(x, y));
+                    }
+                    y++;
+                }
+                x++;
+            }
+        }
+
+        private bool IsBoardSolved()
+        {
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    if (grid[x, y].value == -1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public Cell[,] Solve()
+        {
+            while (!IsBoardSolved())
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    for (int y = 0; y < 9; y++)
+                    {
+                        Cell cell = grid[x, y];
+                        cell.SetAvailable();
+                        cell.SolveByScanning();
+                        cell.SolveByMissingNumbers();
+
+                    }
+                }
+            }
+            return grid;
+        }
+    }
+}
